79f8481de6790a91d2f12eee1b0ce6c8eb8ba9fe
Vladimir Arkatov <78140134+Darkink69@users.noreply.github.com>
2025-07-08 10:51:51 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def handle_request():
+    # Получаем параметры из запроса
+    name = request.args.get('name', default='')
+    chat_id = request.args.get('id', default='0')
+    item = request.args.get('item', default='')
+
+    # Формируем ответ
+    response = {
+        name: True,
+        "chatId": int(chat_id),
+        "data": {}
+    }
+
+    return jsonify(response)
 
 @app.route('/about')
 def about():
