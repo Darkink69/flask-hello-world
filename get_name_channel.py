7676bf5eb3892e80ab96a4d0d2cbc98235d50586
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-08-26 10:29:00 +0700

    upload_int_id

--- get_name_channel.py
+++ get_name_channel.py
@@ -2,14 +2,14 @@ import requests
 import json
 
 
-def get_name_channel(site, id_channel):
+def get_name_channel(site, channel):
     url = f'https://api.audioaddict.com/v1/{site}/channels.json'
     try:
         r = requests.get(url)
         channels = r.json()
-        for channel in channels:
-            if channel['id'] == id_channel:
-                # print(channel['id'])
-                return channel['name']
+        for i in channels:
+            if i['id'] == int(channel):
+                # print(i['name'])
+                return i['name']
     except BaseException:
         return []
