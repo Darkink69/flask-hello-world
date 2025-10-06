7ed8ffd2a695c16d6de65cc3fe92cea1ba3fcf26
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-10-06 16:28:54 +0700

    remote_access_0.21v

--- main_one.py
+++ main_one.py
@@ -1,4 +1,6 @@
 import get_name_channel
+import get_random_acсess
+from manual_upload_mp3 import data_link
 
 oauth_token = "y0__xC70devARjblgMg3NzV4xMIZ_XCL_u7KWPy_1nYK9cpmiVNUQ"  # Ваш OAuth-токен
 
@@ -6,7 +8,13 @@ oauth_token = "y0__xC70devARjblgMg3NzV4xMIZ_XCL_u7KWPy_1nYK9cpmiVNUQ"  # Ваш 
 def upload_one_mp3(site=0, channel='none', id_track=0):
     url_ch = f'https://qh8bsvaksadb2kj9.public.blob.vercel-storage.com/{site}/db_{site}_full_{channel}_premium_light.json'
 
+    data_link = False
+
+    if not data_link:
+        data_link = get_random_acсess.get_access_data()
+        print(data_link)
+
     name_channel = get_name_channel.get_name_channel(site, channel)
     print(name_channel)
     print(site, channel, id_track, '!!!')
-    return f'работает {channel}, {site}, {id_track}, {name_channel} качается!'
+    return f'работает {channel}, {site}, {id_track}, {name_channel}\n{data_link}'
