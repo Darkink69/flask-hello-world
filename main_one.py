a16d135a5579a3acc8f7cadd079b8528da88c7e2
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-10-06 14:25:39 +0700

    remote_access_0.17v

--- main_one.py
+++ main_one.py
@@ -1,5 +1,5 @@
 # загрузка по одному, должна быть на сервере
-def upload_one_mp3(site, channel, id_track):
+def upload_one_mp3(site=0, channel='none', id_track=0):
     print("upload_one_mp3!!!")
     print(site, channel, id_track, '!!!')
-    return site, channel, id_track
+    return 'работает!'
